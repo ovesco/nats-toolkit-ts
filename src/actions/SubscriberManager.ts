58003d0d402ef6b4dcf1322eb581aef524926ad4
guillaume hochet <guillaume.hochet@gmail.com>
2022-08-15 20:16:00 +0200

    fix stuff

--- src/actions/SubscriberManager.ts
+++ src/actions/SubscriberManager.ts
@@ -1,24 +1,37 @@
 import { Msg, NatsConnection, Subscription, SubscriptionOptions } from 'nats';
-import { Span } from 'opentracing';
+import { Span, SpanStatusCode } from '@opentelemetry/api';
 import { match } from 'ts-pattern';
-import { TypeOf, ZodSchema } from 'zod';
+import { TypeOf, z, ZodSchema, ZodType } from 'zod';
 import { BrokerConfig } from '../ConfigBuilder';
-import { Envelope, BasePayload } from '../Messaging';
 import BaseManager from './BaseManager';
+import { BaseCallbackPayload, BaseEnvelope } from '../BaseTypes';
 
-export type SubscribePayload<T> = BasePayload<T, Msg>;
+type SubscribeEnvelope<T> = BaseEnvelope & {
+  data: T;
+};
+
+type SubscribePayload<T> = BaseCallbackPayload<Msg, SubscribeEnvelope<T>> & {
+  data: T;
+};
+
 type Subscriber<T, C extends {}> = (data: SubscribePayload<T> & C) => Promise<void> | void;
 
-export type Envelopator<T> = (envelopeMaker: (data: T) => Envelope<T>) => Envelope<T>;
-export type SendPayload<T> = Envelope<T> | Envelopator<T>;
+type Envelopator<T> = (envelopeMaker: (data: T) => SubscribeEnvelope<T>) => SubscribeEnvelope<T>;
+type SendPayload<T> = SubscribeEnvelope<T> | Envelopator<T>;
 
 export type SubscriberDefinitions = Record<string, ZodSchema>;
+
+type SubscriptionDefinition<Z extends ZodType> = {
+  input: Z
+};
 
 class SubscriberManager<S extends SubscriberDefinitions, C extends {} = {}> extends BaseManager<C> {
   
   constructor(connection: NatsConnection, config: BrokerConfig, private subscriptions: S, context: C) {
     super(connection, config, context);
   }
+
+  async sub()
 
   async subscribe<K extends keyof S>(subject: K, subscriber: Subscriber<TypeOf<S[K]>, C>, opts?: SubscriptionOptions): Promise<Subscription> {
     const sub = this.connection.subscribe(subject as string, {
@@ -28,50 +41,39 @@ class SubscriberManager<S extends SubscriberDefinitions, C extends {} = {}> exte
 
     (async (subscription) => {
       for await (const message of subscription) {
-        const payload = super.buildBasicPayload<TypeOf<S[K]>, Msg>(subject, message);
-        const validation = super.validateEnvelope(payload.envelope, ['data', this.subscriptions[subject]]);
-        if (validation !== true) {
-          payload.span.logEvent('validation-error', { error: validation });
-          payload.span.finish();
-          return;
-        }
-
-        try {
-          this.config.logger.info({ subject, type: 'subscribe', start: Date.now() })
-          subscriber(payload);
-        } catch (error) {
-          payload.span.logEvent('subscriber-error', { error });
-        } finally {
-          payload.span.finish();
-        }
+        this.withBasicPayload<Msg, SubscribeEnvelope<TypeOf<S[K]>>>(subject as string, 'subscribe', message, async (payload) => {
+          super.validateEnvelope(payload.envelope, { data: payload.envelope.data, schema: this.subscriptions[subject] }, undefined, true);
+          await subscriber({
+            ...payload,
+            data: payload.envelope.data,
+          });
+        });
       }
     })(sub);
     return sub;
   }
 
   async publish<K extends keyof S>(subject: K, message: SendPayload<TypeOf<S[K]>>, publishSpan?: Span) {
-    const span = super.buildSpan(subject as string, 'publish', publishSpan);
     const envelope = match(typeof message)
-      .with('object', () => message as Envelope<TypeOf<S[K]>>)
+      .with('object', () => message as SubscribeEnvelope<TypeOf<S[K]>>)
       .with('function', () => {
         const envelopator = message as Envelopator<TypeOf<S[K]>>;
         const envelopeMaker = (data: TypeOf<S[K]>) => ({
-          ...super.buildBaseEnvelope(subject as string),
+          ...super.buildBaseEnvelope(subject as string, publishSpan),
           data,
         });
         
         return envelopator(envelopeMaker);
       }).run();
 
-    const validation = super.validateEnvelope(envelope, ['data', this.subscriptions[subject]]);
+    const validation = super.validateEnvelope(envelope, { data: envelope.data, schema: this.subscriptions[subject] }, undefined);
     if (validation !== true) {
+      publishSpan?.recordException(validation);
+      publishSpan?.setStatus({ code: SpanStatusCode.ERROR });
       throw validation;
     }
-
-    const headers = super.buildMessageHeaders(span);
     const payload = this.config.serializer(envelope);
-    this.connection.publish(subject as string, payload, { headers });
-    span.finish();
+    this.connection.publish(subject as string, payload);
   }
 }
 
